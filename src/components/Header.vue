1043e846f7d66e8e525f8f96d05d5143717d5b06
andres gonzalez <andres.gonzalez@cluvi.com>
2024-03-22 19:30:50 -0500

    final proyecto base

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { computed } from 'vue';
 const props = defineProps({
     carrito:{
         type:Array,
         required:true
+    },
+    guitarra:{
+        type:Object,
+        required:true
     }
 })
-defineEmits(['decrementar-carrito','incrementar-carrito'])
+defineEmits(['decrementar-carrito','incrementar-carrito','agregar-carrito', 'eliminar-producto', 'vaciar-carrito'])
+const totalPagar = computed(()=>{
+    return props.carrito.reduce((total, producto)=> total + (producto.cantidad * producto.precio), 0)
+})
 </script>
 <template>
     <header class="py-5 header">
@@ -70,6 +78,7 @@ defineEmits(['decrementar-carrito','incrementar-carrito'])
                                                 <button
                                                     class="btn btn-danger"
                                                     type="button"
+                                                    @click="$emit('eliminar-producto', producto.id)"
                                                 >
                                                     X
                                                 </button>
@@ -77,8 +86,10 @@ defineEmits(['decrementar-carrito','incrementar-carrito'])
                                         </tr>
                                     </tbody>
                                 </table>
-                                <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
-                                <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
+                                <button class="btn btn-dark w-100 mt-3 p-2"
+                                @click="$emit('vaciar-carrito')"
+                                >Vaciar Carrito</button>
                             </div>
                         </div>
                     </div>
@@ -87,12 +98,13 @@ defineEmits(['decrementar-carrito','incrementar-carrito'])
 
             <div class="row mt-5">
                 <div class="col-md-6 text-center text-md-start pt-5">
-                    <h1 class="display-2 fw-bold">Modelo VAI</h1>
-                    <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio exercitationem eos inventore odit.</p>
-                    <p class="text-primary fs-1 fw-black">$399</p>
+                    <h1 class="display-2 fw-bold">Modelo {{ guitarra.nombre }}</h1>
+                    <p class="mt-5 fs-5 text-white">{{ guitarra.descripcion }}</p>
+                    <p class="text-primary fs-1 fw-black">${{ guitarra.precio }}</p>
                     <button 
                         type="button"
                         class="btn fs-4 bg-primary text-white py-2 px-5"
+                        @click="$emit('agregar-carrito', guitarra)"
                     >Agregar al Carrito</button>
                 </div>
             </div>
